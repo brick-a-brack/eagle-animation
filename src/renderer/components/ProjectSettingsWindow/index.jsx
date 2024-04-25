97882333602b09ef61963dcd9ff95f339c6e37f0
Maxime Baconnais <m.baconnais@outlook.com>
2024-04-25 11:33:44 +0200

    Add ratio titles

--- src/renderer/components/ProjectSettingsWindow/index.jsx
+++ src/renderer/components/ProjectSettingsWindow/index.jsx
@@ -28,13 +28,13 @@ const ProjectSettingsWindow = ({ t, onProjectSettingsChange = () => {}, onProjec
 
   const RATIOS = [
     { value: '', label: t('Automatic') },
-    { value: '4:3', label: '4:3' }, // 1.33
-    { value: '16:9', label: '16:9' }, // 1.77
-    { value: '1.85:1', label: '1.85' }, // 1.85
-    { value: '2.39:1', label: '2.39' }, // 2.39
-    { value: '1:1', label: '1' }, // 1
-    { value: '9:16', label: '9:16' }, // 0.56
-    { value: '3:4', label: '3:4' }, // 0.75
+    { value: '4:3', label: '4:3 ' + t('(Old TV)') }, // 1.33
+    { value: '16:9', label: '16:9 ' + t('(TV)') }, // 1.77
+    { value: '1.85:1', label: '1.85:1 ' + t('(Cinema)') }, // 1.85
+    { value: '2.39:1', label: '2.39:1 ' + t('(Cinema)') }, // 2.39
+    { value: '1:1', label: '1:1 ' + t('(Square)') }, // 1
+    { value: '9:16', label: '9:16 ' + t('(Story)') }, // 0.56
+    { value: '3:4', label: '3:4 ' + t('(Social media)') }, // 0.75
     { value: 'custom', label: t('Custom') },
   ];
 
@@ -73,13 +73,13 @@ const ProjectSettingsWindow = ({ t, onProjectSettingsChange = () => {}, onProjec
       <FormGroup label={t('Animation framerate')} description={t('The framerate used for your animation')}>
         <NumberInput style={{ marginLeft: 'var(--space-big)' }} min={1} max={60} tag={t('FPS')} register={register('fps')} />
       </FormGroup>
-      <FormGroup label={t('Animation ratio')} description={t('The ratio of your animation')}>
+      <FormGroup label={t('Aspect ratio')} description={t('The aspect ratio of your animation')}>
         <div style={{ display: 'inline-block' }}>
           <Select options={RATIOS} control={control} register={register('ratio')} />
         </div>
         {watch('ratio') === 'custom' && (
           <div style={{ display: 'inline-block' }}>
-            <NumberInput style={{ marginLeft: 'var(--space-big)' }} min={0} max={8} step={0.01} register={register('customRatio')} />
+            <NumberInput style={{ marginLeft: 'var(--space-big)' }} min={0} max={5} step={0.01} register={register('customRatio')} />
           </div>
         )}
       </FormGroup>
