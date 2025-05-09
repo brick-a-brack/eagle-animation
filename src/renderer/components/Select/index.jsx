afe394d018e1d9a7c6f936d020087d393758be20
Maxime Baconnais <m.baconnais@outlook.com>
2025-05-09 15:48:52 +0200

    feat: Improve Select component to support groups and disabled status

--- src/renderer/components/Select/index.jsx
+++ src/renderer/components/Select/index.jsx
@@ -5,10 +5,18 @@ import * as style from './style.module.css';
 const Select = ({ register = {}, options = [], ...rest }) => {
   return (
     <select className={style.select} {...rest} {...register}>
-      {options.map((o) => (
-        <option key={o.value} value={o.value}>
+      {options.map((o) => (<>
+        {o.values && <optgroup label={o.label} id={o.id}>
+          {o.values.map((v) => (
+            <option key={v.value} value={v.value} disabled={v.disabled || false}>
+              {v.label}
+            </option>
+          ))}
+        </optgroup>}
+        {!o.values && <option key={o.value} value={o.value} disabled={o.disabled || false}>
           {o.label}
-        </option>
+        </option>}
+      </>
       ))}
     </select>
   );
