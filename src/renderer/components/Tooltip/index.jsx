8a5994122474fe873aa6b31955468df9071ee8d2
Maxime Baconnais <m.baconnais@outlook.com>
2026-06-11 17:15:39 +0200

    feat: Disable tooltips on touchable screens

--- src/renderer/components/Tooltip/index.jsx
+++ src/renderer/components/Tooltip/index.jsx
@@ -2,6 +2,11 @@ import { Tooltip as ReactTooltip } from 'react-tooltip';
 
 import * as style from './style.module.css';
 
-const Tooltip = ({ ...rest }) => <ReactTooltip className={style.tooltip} delayShow={0} delayHide={0} offset={15} {...rest} />;
+const isTouchPrimary = window.matchMedia('(hover: none) and (pointer: coarse)').matches;
+
+const Tooltip = ({ ...rest }) => {
+  if (isTouchPrimary) return null;
+  return <ReactTooltip className={style.tooltip} delayShow={0} delayHide={0} offset={15} {...rest} />;
+};
 
 export default Tooltip;
