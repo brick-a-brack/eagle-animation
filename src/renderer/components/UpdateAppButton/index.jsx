1aaa6f7ef66544ab1927aa2156ddd9f2104169fd
Maxime Baconnais <m.baconnais@outlook.com>
2026-07-10 22:18:27 +0200

    feat: Add version hash on setting update button

--- src/renderer/components/UpdateAppButton/index.jsx
+++ src/renderer/components/UpdateAppButton/index.jsx
@@ -5,10 +5,10 @@ import { withTranslation } from 'react-i18next';
 import * as style from './style.module.css';
 
 const UpdateBanner = ({ t }) => {
-  const { currentVersion, latestVersion, canBeUpdated, actions } = useAppVersion();
+  const { currentVersion, latestVersion, canBeUpdated, build, actions } = useAppVersion();
 
   return (
-    <Action onClick={canBeUpdated ? actions.openUpdatePage : undefined} role="button" tabIndex={canBeUpdated ? 0 : -1} className={`${style.button} ${canBeUpdated ? '' : style.disabled}`}>
+    <Action title={build} onClick={canBeUpdated ? actions.openUpdatePage : undefined} role="button" tabIndex={canBeUpdated ? 0 : -1} className={`${style.button} ${canBeUpdated ? '' : style.disabled}`}>
       {canBeUpdated ? t('Update to {{version}}', { version: latestVersion }) : t('Already up to date ({{version}})', { version: currentVersion || t('Unknown') })}
     </Action>
   );
